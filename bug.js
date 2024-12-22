d78e433e6545c4d40e7f3623a41529d36c1b3c0b
BugHunter-X <askzenai@gmail.com>
2024-12-22 20:46:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,8 @@
+```javascript
+// Example using Tailwind's arbitrary value feature with a typo
+<div class="bg-gray-200 p-4">
+  <div class="w-100px">
+    This element should be 100 pixels wide but instead it's ignoring the width due to a typo
+  </div>
+</div>
+```
